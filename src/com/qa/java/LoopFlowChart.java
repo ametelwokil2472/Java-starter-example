2b2828ae01c5c242abe3e6669ef38384ffc3300b
Ametelwokil <Ametelwokil@DESKTOP-I21TU9P.lan>
2021-05-18 00:02:31 -0500

    AO: 3rd Conditionals, iteration, Array

--- src/com/qa/java/LoopFlowChart.java
+++ src/com/qa/java/LoopFlowChart.java
@@ -7,8 +7,9 @@ public class LoopFlowChart {
      for ( int A = 100; A < 200; A++) {
     	 System.out.println(A);
      }
-     getTenTims();
+     printValueNumber();
      flowChart2();
+     printNumber();
 	}
      
      public static void flowChart2() {
@@ -23,20 +24,47 @@ public class LoopFlowChart {
  	   	  
       
  }
+
+
+     public static void printNumber() {
+ 		
+		 for (int A=1;A<=10;A++) {
+			for (int B=1;B<=10;B++) //print 1 up to 10 10 times
+			// for (int B=1;B<=A;B++) //print 1 up to 10 B times
+			 {
+	    	  System.out.println(A);
+	    		       }
+}  
+	   	  
+     
+}
+     public static void printValueNumber() {
+  		
+ 		 for (int A=1;A<=10;A++) {
+ 			//for (int B=1;B<=10;B++) //print 1 up to 10 10 times
+ 			for (int B=1;B<=A;B++) //print 1 up to 10 B times
+ 			 {
+ 	    	  System.out.println(A);
+ 	    		       }
+ }  
+ 	   	  
+      
+ }
+
 	
-	public static void getTenTims() {
-		int  iteration = 1;
-	      while(iteration<=10)
-	       {
-	           int count= 1;
-	           while(count<=10)
-	           {
-	               System.out.print(count + " ");
-	               count++;
-	           }
-	           System.out.print("\n");
-	           iteration++;
-	       }
-	    }
+//	public static void getTenTims() {
+//		int  iteration = 1;
+//	      while(iteration<=10)
+//	       {
+//	           int count= 1;
+//	           while(count<=10)
+//	           {
+//	               System.out.print(count + " ");
+//	               count++;
+//	           }
+//	           System.out.print("\n");
+//	           iteration++;
+//	       }
+//	    }
 	}
 	
